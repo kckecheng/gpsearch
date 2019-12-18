10b3234126138ac5312d9016ed45b7368dc87539
Kecheng Bi <bikecheng@gmail.com>
2019-12-18 21:38:18 +0800

    use map instead of struct during unmarshal

--- search.go
+++ search.go
@@ -3,66 +3,48 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 )
 
-const goDocEP = "https://api.godoc.org/search?q="
-
-// Error code for abnormal exist
+// Const definition
 const (
-	ErrGet   = 1
-	ErrParse = 2
+	goDocEP = "https://api.godoc.org/search?q=" // // godoc.org API endpoint
+	ErrExit = 1                                 // ErrExit code for abnormal exist
 )
 
-type queryResp struct {
-	Results []struct {
-		Name     string  `json:"name"`
-		Path     string  `json:"path"`
-		Import   int32   `json:"import_count"`
-		Synopsis string  `json:"synopsis"`
-		Starts   int32   `json:"stars"`
-		Score    float32 `json:"score"`
-	} `json:"results"`
-}
-
-func parseArgs() {
-	var (
-		n int    // Num. of records to show
-		q string // query string
-		f string // Format: table, list
-		d bool   // Show description
-		u bool   // Show num. of projects use this package
-		h bool   // Show starts
-		s bool   // Show score
-	)
-}
-
-func main() {
-	uri := "micro/go-micro"
-	url := goDocEP + fmt.Sprintf("\"%s\"", uri)
-
+// Query Result shares below format:
+// {
+//     "results": [
+//         {
+//             "path": "github.com/micro/go-micro/client",
+//             "import_count": 0,
+//             "synopsis": "Package client is an interface for an RPC client"
+//         }
+//     ]
+// }
+func query(s string) ([]map[string]interface{}, error) {
+	url := goDocEP + fmt.Sprintf("%s", s)
 	resp, err := http.Get(url)
 	if err != nil {
-		log.Printf("Failt to query %s\n", uri)
-		os.Exit(ErrGet)
+		return nil, err
 	}
 	defer resp.Body.Close()
 
 	decoder := json.NewDecoder(resp.Body)
-	data := queryResp{}
+	data := map[string][]map[string]interface{}{}
 	err = decoder.Decode(&data)
 	if err != nil {
-		log.Printf("Fail to parse query results")
-		os.Exit(ErrParse)
+		return nil, err
 	}
-	for _, record := range data.Results {
-		fmt.Printf("Name: %s\n", record.Path)
-		fmt.Printf("Description: %s\n", record.Synopsis)
-		fmt.Printf("Used by: %d\n", record.Import)
-		fmt.Printf("Starts: %d\n", record.Starts)
-		fmt.Printf("Score: %.2f\n", record.Score)
-		fmt.Println()
+	return data["results"], nil
+}
+
+func main() {
+	pkgs, err := query("micro")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(ErrExit)
 	}
+	fmt.Printf("%+v", pkgs)
 }
