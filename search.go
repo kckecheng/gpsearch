75b7cfff273426de161c842db4e73bc7aae5d71f
Kecheng Bi <bikecheng@gmail.com>
2019-12-19 19:37:46 +0800

    release 1.0.1

--- search.go
+++ search.go
@@ -116,34 +116,50 @@ func sortPkgs(pkgs []map[string]interface{}, field string) {
 
 func main() {
 	app := &cli.App{
-		Name:        "gpsearch",
-		Usage:       "search golang packages",
-		Description: "search golang packages on godoc.org from CLI",
-		Version:     "1.0.0",
+		Name:  "gpsearch",
+		Usage: "search golang packages",
+		UsageText: `
+			search [--sort|-s <field>] [--reverse|-r] [--num|-n <Num. of packages to list>] \
+				[[--fields|-f <field1>] [--fields|-f <field2>] [...]] <pattern1> [pattern2] [...]`,
+		Description: `
+			Search golang packages on godoc.org from CLI
+
+			Fields supported:
+				name        : The name
+				path        : The import path
+				synopsis    : The description
+				import_count: Num. of projects using/importing this
+				stars       : Num. of github stars
+				score       : How well is the document
+				fork        : If this is a forked repository
+
+			Notes: Some packages only have few fields with data
+		`,
+		Version: "1.0.1",
 		Flags: []cli.Flag{
 			&cli.StringFlag{
 				Name:    "sort",
 				Aliases: []string{"s"},
 				Value:   "import_count",
-				Usage:   "sort packages based on",
+				Usage:   "Sort packages based on",
 			},
 			&cli.BoolFlag{
 				Name:    "reverse",
 				Aliases: []string{"r"},
 				Value:   false,
-				Usage:   "reverse the sort result",
+				Usage:   "Reverse the sort result",
 			},
 			&cli.Uint64Flag{
 				Name:    "num",
 				Aliases: []string{"n"},
 				Value:   10,
-				Usage:   "num. of packages to list",
+				Usage:   "Num. of packages to list",
 			},
 			&cli.StringSliceFlag{
 				Name:    "fields",
 				Aliases: []string{"f"},
 				Value:   cli.NewStringSlice("path", "import_count", "synopsis"),
-				Usage:   "package fields to show, speficie multiple values with -f <field1> -f <field2> ...",
+				Usage:   "Package fields to show, speficie multiple values with -f <field1> -f <field2> ...",
 			},
 		},
 		Action: func(c *cli.Context) error {
